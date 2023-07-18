198b18e2a2c99775a1fa90004516c063e99933a0
MartaBelmonte <martabelmonts5@gmail.com>
2023-07-18 09:54:50 +0200

    login

--- web/src/services/api-movies.js
+++ web/src/services/api-movies.js
@@ -1,14 +1,24 @@
-// login
-
+//login
 const getMoviesFromApi = (params) => {
   console.log('Se están pidiendo las películas de la app');
-console.log(params);
-  // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch(`//localhost:4000/movies/?genre=${params.genre}&sort=${params.sort}`)
-    .then((response) => response.json())
+  console.log(params);
+
+  const url = `http://localhost:4000/movies/?genre=${params.genre}&sort=${params.sort}`;
+  
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+      return response.json();
+    })
     .then((data) => {
-      console.log('getMoviesFromApi: ' + data);
+      console.log('getMoviesFromApi:', data);
       return data;
+    })
+    .catch((error) => {
+      console.error('Error al obtener las películas:', error);
+      throw error; // Puedes manejar el error en la capa superior o realizar acciones específicas aquí
     });
 };
 
@@ -17,3 +27,4 @@ const objToExport = {
 };
 
 export default objToExport;
+
